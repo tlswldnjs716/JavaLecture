140d6073ea25c284fd29b9c0d88d5f4036e1c061
JIWON <tlswldnjs716@naver.com>
2022-03-14 11:49:51 +0900

    cmt

---
+++ java-workspace/JAVA/src/homework/Work7New.java
@@ -0,0 +1,17 @@
+package homework;
+
+public class Work7New {
+	public static void main(String[] args) {
+//		for(int dan=2;dan<=9; dan+=3) {
+//			for(int i=1; i<=9; i++) {
+//				System.out.printf("%d * %d = %d \t",dan,i,dan*i);
+//				System.out.printf("%d * %d = %d \t",dan+1,i,(dan+1)*i);
+//				if(dan+2<10) {
+//				System.out.printf("%d * %d = %d",dan+2,i,(dan+2)*i);
+//				}
+//				System.out.println();
+//			}
+//			System.out.println();
+//		}
+	}
+} 
